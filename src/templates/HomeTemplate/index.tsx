80f0d9fb9b95acd301a04b2107d6e0a19002c9ed
Maskalenkas <maskalenkasdoc@gmail.com>
2022-09-09 01:00:51 -0300

    feat: add eslint configs

--- src/templates/HomeTemplate/index.tsx
+++ src/templates/HomeTemplate/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import style from './styles.module.scss'
-
-import Button from '../../components/atoms/Button'
+import { Button } from '../../components/atoms/Button'
 
 const HomeTemplate = () => (
+
   <main data-component-home-template={true} className={style.wrapper}>
     <Button/>
   </main>
