73ebecd7c77174fe81ef12d474d42869af3ff641
Maskalenkas <maskalenkasdoc@gmail.com>
2022-09-09 00:42:52 -0300

    refactor: att generators

---
+++ src/components/atoms/Teste/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+export const Teste = () => {
+  return (
+    <div data-component-teste={true} className={styles.container}>
+      teste
+    </div>
+  )
+}
