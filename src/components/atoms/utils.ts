d641bd2a4922eaac8bdb99c797a1b7f44b70fd86
Maskalenkas <maskalenkasdoc@gmail.com>
2022-10-16 00:17:31 -0300

    fix

--- src/components/atoms/utils.ts
+++ src/components/atoms/utils.ts
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react'
 
-export type AtomsDefaultProps<VariantType = void, TagType = keyof HTMLElementTagNameMap> = {
+export type AtomsDefaultProps<VariantType = 'default', TagType = keyof HTMLElementTagNameMap> = {
 	as?: TagType
 	className?: string
 	children?: ReactNode
